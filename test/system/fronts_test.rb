4ace8bd1090ed3bcc1057272eedddb5a902f20e9
Kapil Narotam <kapilnarotam@gmail.com>
2018-12-19 16:57:44 +0200

    Initial commit

---
+++ test/system/fronts_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class FrontsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit fronts_url
+  #
+  #   assert_selector "h1", text: "Front"
+  # end
+end
