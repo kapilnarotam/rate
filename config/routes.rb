1a620ffbed99ade7e16712df3244b5c2b08c6dd1
Kapil Narotam <kapilnarotam@gmail.com>
2018-12-21 15:46:28 +0200

    added bootstrap to calculator, centered exchange rate tables, generated FAQs scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :faqs
   resources :calculators
   resources :rats
 	root 'fronts#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181220093028) do
+ActiveRecord::Schema.define(version: 20181221124818) do
 
   create_table "calculators", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "faqs", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
